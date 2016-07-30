07686e51b904887f9ab06d4ad8778e70bb496321
Fernando Xavier <nandogimenes@gmail.com>
2016-07-30 18:19:08 -0300

    Initial Commit

---
+++ AgendaDeContatos/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  AgendaDeContatos
+//
+//  Created by Fernando Xavier on 7/30/16.
+//  Copyright © 2016 Slackfx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ AgendaDeContatos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AgendaDeContatos
+//
+//  Created by Fernando Xavier on 7/30/16.
+//  Copyright © 2016 Slackfx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
