d03eded25514e1ab822e334e2d2e1c925c32d782
slackfx <nandogimenes@gmail.com>
2016-07-31 10:56:59 -0300

    Adicionado um DAO para gerenciar a lista de contatos, esta também implementa o pattern Singleton

---
+++ AgendaDeContatos/ContatoDAO.h
@@ -0,0 +1,13 @@
+//
+//  ContatoDAO.h
+//  AgendaDeContatos
+//
+//  Created by Fernando Xavier on 7/31/16.
+//  Copyright © 2016 Slackfx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ContatoDAO : NSObject
+
+@end
---
+++ AgendaDeContatos/ContatoDAO.m
@@ -0,0 +1,13 @@
+//
+//  ContatoDAO.m
+//  AgendaDeContatos
+//
+//  Created by Fernando Xavier on 7/31/16.
+//  Copyright © 2016 Slackfx. All rights reserved.
+//
+
+#import "ContatoDAO.h"
+
+@implementation ContatoDAO
+
+@end
